bcdfb59608180176b2f82a751b7798a29c021148
RMTV US <rmtvus@gmail.com>
2021-01-09 21:43:00 +0000

    I made a python pizza dilivery

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+S = 15
+M = 20
+L = 25
+N = 0
+Y = 2
+if size == S:
+  print("15")
+if size == M:
+  print("$20")
+if size == L:
+  print("$25")
+
+if add_pepperoni == Y:
+  S += 2
+  M += 2
+  L += 2
+ 
+if extra_cheese == Y:
+  S += 2
+
+print(f"your total {S}")
+
 
 
 
